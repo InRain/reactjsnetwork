b565dbd7aa01a7ffce2e2b0449330e7426f86b3f
Mikhail <ilyinmi1993@gmail.com>
2020-08-08 12:39:37 +0300

    new commit:wq

---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src='https://cs9.pikabu.ru/post_img/2017/09/18/4/og_og_1505707767281929590.jpg'/>
+            <div>ava + descr</div>
+            <div>My posts
+                <div>New post</div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Profile;
