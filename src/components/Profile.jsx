bfaaae939543e11f93bee124d6c6b3c042ac33ef
Mikhail <ilyinmi1993@gmail.com>
2020-08-08 13:14:54 +0300

    added css modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <img src='https://cs9.pikabu.ru/post_img/2017/09/18/4/og_og_1505707767281929590.jpg'/>
             <div>ava + descr</div>
             <div>My posts
                 <div>New post</div>
-                <div>
-                    <div>post1</div>
+                <div className={classes.posts}>
+                    <div className={classes.postItem}>post1</div>
                     <div>post2</div>
                 </div>
             </div>
