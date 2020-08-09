8dae47f95f961a262f1d141ec042375c729d8c2b
Mikhail <ilyinmi1993@gmail.com>
2020-08-09 23:23:41 +0300

    small fix

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -4,8 +4,7 @@ import classes from './ProfileInfo.module.css'
 const ProfileInfo = () => {
     return (
         <div>
-            <div>
-                <img src='https://cs9.pikabu.ru/post_img/2017/09/18/4/og_og_1505707767281929590.jpg'/>
+            <div className={classes.profileHeader}>
             </div>
             <div className={classes.descriptionBlock}>ava + descr</div>
         </div>
